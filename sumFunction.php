c080a446d28647d3ba0eca0af7e7bc1f4daed53d
Kamil Jankowski <kjankowski85@gmail.com>
2019-11-18 16:47:05 +0000

    html and php file upload, sumFunction sums the digits fromthe provided number

---
+++ sumFunction.php
@@ -0,0 +1,21 @@
+<?php
+ 
+function sumDigits(int $num) {
+	$numlength = strlen((string)$num);
+	$string = (string)$num;
+	$result = 0;
+	for($i=0;$i<$numlength;$i++){
+		$result += $string[$i];
+   	}
+	echo "Your result is: ";
+	echo $result;
+  	}
+if(isset($_POST['number'])){
+  $num=$_POST['number'];
+  sumDigits($num);
+}
+else{
+	echo "Please go back to the from.";
+}
+
+?>
