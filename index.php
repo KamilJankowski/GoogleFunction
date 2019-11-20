a4a9afc70c840bef214b79acdcf6ddeb5b780509
Kamil Jankowski <kjankowski85@gmail.com>
2019-11-20 15:45:32 +0000

    add comments to the file

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <?php
-echo "<html>
-<body>
-<h3>Please enter your number</h3>
-<form action='sumFunction.php' method='post'>
-Number: <input type='number' name='number'><br>
-<input type='submit'>
-</form>
+ //This function will split number to digits and sum them
+function sumDigits(int $num) {
+	$numlength = strlen((string)$num);
+	$string = (string)$num;
+	$result = 0;
+	for($i=0;$i<$numlength;$i++){
+		$result += $string[$i];
+   	}
+	echo "Your result is: ";
+	echo $result;
+ }
+	//Display form
+	echo "<html>
+        <body>
+        <h3>Please enter your number</h3>
+        <form action='' method='post'>
+        Number: <input type='number' name='number'><br>
+        <input type='submit'>
+        </form>
+        </body>
+        </html>";
 
-</body>
-</html>";
+//Read the number and display the result
+if(isset($_POST['number'])){
+  $num=$_POST['number'];
+  sumDigits($num);
+}
+else{
+	echo "No result.";
+}
+
+?>
