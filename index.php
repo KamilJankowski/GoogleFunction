3be72a883dd2f80f75463304a45c9208fe98e38c
Kamil Jankowski <kjankowski85@gmail.com>
2019-11-20 15:33:57 +0000

    display html form

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo " New hello world!";
+echo "<html>
+<body>
+<h3>Please enter your number</h3>
+<form action='sumFunction.php' method='post'>
+Number: <input type='number' name='number'><br>
+<input type='submit'>
+</form>
+
+</body>
+</html>";
